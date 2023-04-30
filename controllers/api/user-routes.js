0e197aa251a287352750bb688318d65abb2bed30
Travis <traviscmiller01@gmail.com>
2023-04-30 01:22:23 -0700

    user signup route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Signup Route
+router.post('/', async (req, res) => {
+    try{
+        const userData = await User.create(req.body);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true;
+            
+            res.status(200).json(userData);
+        });
+    }
+    catch (err) {
+        res.status(400).json(err);
+    }
+});
 // Login Route
 router.post('/login', async (req, res) => {
     try{
