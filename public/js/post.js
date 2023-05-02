13e9ebb323f810e6a541171794678c3a10c8d005
Travis <traviscmiller01@gmail.com>
2023-05-02 04:29:16 -0700

    added modal to dashboard

--- public/js/post.js
+++ public/js/post.js
@@ -1,4 +1,24 @@
 const newPostHandler = async (event) => {
     event.preventDefault();
-
+    const title = document.querySelector('#newPostTitle').value.trim();
+    const content = document.querySelector('#newPostContent').value.trim();
     
+
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response)
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        }
+        else {
+            alert(response.statusText);
+        }
+    
+};
+
+document
+    .getElementById('newPostSubmit')
+    .addEventListener('submit', newPostHandler);
